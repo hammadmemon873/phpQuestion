5433d9dd472feb007ed115cfea6aadfdd9cd1f8b
hammad memon <hammadmemon873@gmail.com>
2024-02-17 15:28:24 +0500

    table work

---
+++ table.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<TABLE BORDER=2 CELLPADDING=4>
+<TR>
+<TD COLSPAN=4 BGCOLOR=“skyblue" align="center" > Students Information 
+</TD>
+</TR>
+<TR>
+    <Th ROWSPAN=1 BGCOLOR="#99CCFF">PHP</Th>
+    <TD>1153</TD>
+    <TD>Ahmed</TD>     
+    <TD>Morning</TD>
+ </TR>
+<TR>
+    <TD>2785</TD>
+    <TD>Asad</TD>
+    <TD>Afternoon</TD>
+</TR>
+<TR>
+     	<TD>110</TD>
+	<TD>Manzoor</TD> 	<TD>Evening</TD>
+ </TR>
+ <TR>
+    <TH ROWSPAN=1 BGCOLOR="#99CCFF">
+	System     &nbsp; &nbsp; &nbsp;    &nbsp;
+   </TH>
+   <TD>1566</TD><TD>Tariq</TD> <TD>Morning</TD>
+</TR>
+<TR>
+    <TD>784</TD><TD>Waheed</TD> <TD>Afternoon</TD>
+</TR>
+<TR>
+    <TD>222</TD><TD>Saleem</TD> <TD>Evening</TD>
+    </TR>
+</TABLE>
+
+</body>
+</html>
