d6c1ddcd06fdc95245c78e487d06af1e7cdad50d
hammad memon <hammadmemon873@gmail.com>
2024-02-17 15:17:04 +0500

    add 2 files

---
+++ 21534_hammad_tableNo.php
@@ -0,0 +1,16 @@
+<?php
+$tableNo =5;
+echo "Multiplication Table of " . $tableNo. "<br>";
+$startFrom = 1;
+$endAt =10;
+
+for($i = $startFrom ; $i<= $endAt;  $i++)
+{
+	echo $tableNo ." X ". $i . " = " . $tableNo*$i. "<br>";	
+
+}
+
+
+
+
+?>
